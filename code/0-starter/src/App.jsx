b8422a42c8a3b37df0c06cf29c334865a19685c9
maria.rodrigues <maria.rodrigues@take.net>
2023-07-19 21:39:43 -0300

    0-starter

---
+++ code/0-starter/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RecipeTitle from './RecipeTitle'
+import './index.css'
+
+function App() {
+    return (
+        <article>
+            <h1> Gerenciador de Receita</h1>
+            <RecipeTitle />
+        </article>
+    )
+}
+
+export default App;
---
+++ code/0-starter/src/RecipeTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RecipeTitle() {
+    const title = 'Purê de batata';
+    return (
+        <h2>{ title }</h2>
+    )
+};
+export default RecipeTitle;
