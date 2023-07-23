444d9fc83ba6a3e05edf682f687deb4332d3fc8a
maria.rodrigues <maria.rodrigues@take.net>
2023-07-23 19:13:43 -0300

    Mashed potatoes

--- code/2-component-data/start/src/RecipeTitle.jsx
+++ code/2-component-data/start/src/RecipeTitle.jsx
@@ -1,3 +1,11 @@
 import React from 'react';
 
-// TODO: Create RecipeTitle component
+function RecipeTitle(props) {
+    return (
+        <section>
+            <h2>{ props.title }</h2>
+        </section>
+    )
+};
+
+export default RecipeTitle;
